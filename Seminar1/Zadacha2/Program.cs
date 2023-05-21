696eb5b22f19794d9783a1e5418944bb05e27e73
Vyacheslav <vyacheslavkargapolov83@gmail.com>
2023-05-21 22:51:49 +0500

    добавлено решение задачи2

--- Seminar1/Zadacha2/Program.cs
+++ Seminar1/Zadacha2/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите 2 числа: ");
+Console.Write("a = ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("b = ");
+int numberB = int.Parse(Console.ReadLine());
+if (numberA > numberB)
+{
+    Console.WriteLine($"MAX = {numberA}, MIN = {numberB}");
+}
+else
+{
+    Console.WriteLine($"MAX = {numberB}, MIN = {numberA}");
+}
