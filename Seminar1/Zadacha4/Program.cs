b815c6dcf766b4c7069f09131b3bc405a46cd255
Vyacheslav <vyacheslavkargapolov83@gmail.com>
2023-05-21 23:24:42 +0500

    добавлено условие и решение задачи4

---
+++ Seminar1/Zadacha4/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.*/
+Console.Clear();
+Console.WriteLine("Введите 3  числа: ");
+Console.Write("a = ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("b = ");
+int numberB = int.Parse(Console.ReadLine());
+Console.Write("c = ");
+int numberC = int.Parse(Console.ReadLine());
+if (numberA > numberB)
+{
+    if (numberA > numberC)
+    {
+        Console.WriteLine($"MAX = {numberA}");
+    }
+    else
+    {
+        Console.WriteLine($"MAX = {numberC}");
+    }
+}
+else
+{
+    if (numberB > numberC)
+    {
+        Console.WriteLine($"MAX = {numberB}");
+    }
+    else
+    {
+        Console.WriteLine($"MAX = {numberC}");
+    }
+}
