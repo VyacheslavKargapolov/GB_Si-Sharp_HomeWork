8f64fc64f392fc2217eef9e78dcf109fc53d6231
Vyacheslav <vyacheslavkargapolov83@gmail.com>
2023-05-24 00:13:28 +0500

    reshenie

--- Seminar2/Zadacha10/Program.cs
+++ Seminar2/Zadacha10/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+int result = new Random().Next(100, 1000);
+Console.WriteLine(result);
+int result1=result / 10;
+int result2=result1 % 10;
+Console.WriteLine($"{result2}");
