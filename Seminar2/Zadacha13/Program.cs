a64c38751f1b9e924b38d8014829356c44a87d83
Vyacheslav <vyacheslavkargapolov83@gmail.com>
2023-05-24 13:03:26 +0500

    reshenie + uslovie

--- Seminar2/Zadacha13/Program.cs
+++ Seminar2/Zadacha13/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Clear();
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+
+if (a / 100 == 0)
+{
+    Console.WriteLine($"{a} третьей цифры нет");
+}
+else
+{while (a / 1000 != 0){
+
+    a = a / 10;
+}
+Console.WriteLine($"третья цифра {a % 10}");
+}
