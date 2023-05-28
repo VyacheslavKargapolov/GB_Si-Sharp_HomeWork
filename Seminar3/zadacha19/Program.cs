8df3c55b83b4b70d37576b81e91dbc0707f83062
Vyacheslav <vyacheslavkargapolov83@gmail.com>
2023-05-29 00:09:37 +0500

    dobavil reshenie z19 представил как строку

--- Seminar3/zadacha19/Program.cs
+++ Seminar3/zadacha19/Program.cs
@@ -3,3 +3,8 @@
 12821 -> да
 23432 -> да
 */
+Console.Clear();
+Console.Write("Введите целое 5-ти значное число: ");
+string N = Console.ReadLine();
+if (N[0] == N[4] && N[1] == N[3]) Console.WriteLine($"Число  {N} - палиндром");
+else Console.WriteLine($"Число  {N} - не является палиндромом");
