0571f59102be2358d4420471492b9e07d1769007
Vyacheslav <vyacheslavkargapolov83@gmail.com>
2023-06-02 20:26:35 +0500

    reshenie zadachi27

---
+++ Seminar4/Zadacha27/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 27 Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int MathPow(int b)
+{
+    int sum = 0;
+    int i = 0;
+    while (b/10 > 0)
+    {
+        i = b % 10;
+        sum = sum + i;
+        b = b / 10;
+    }
+    sum =sum + b%10;
+    return sum; 
+}
+
+Console.Clear();
+Console.Write("введите число ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine($"сумма цифр числа {num} равна {MathPow(num)}");
