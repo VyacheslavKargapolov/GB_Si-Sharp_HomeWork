738085f0d0db72b18961fb585b9a15ec8b8ddfaf
Vyacheslav <vyacheslavkargapolov83@gmail.com>
2023-06-02 20:01:25 +0500

    reshenie zadachi25

---
+++ Seminar4/Zadacha25/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+int MathPow(int a, int b)
+{
+    int rez = a;
+    for(int i = 1; i != b; i++)
+    {
+        rez = rez * a;
+    }
+    return rez;
+}
+
+Console.Clear();
+Console.Write("vvedite chislo A: ");
+int numA = int.Parse(Console.ReadLine()!);
+Console.Write("vvedite chislo B: ");
+int numB = int.Parse(Console.ReadLine()!);
+
+if (numB < 0) 
+{
+    Console.WriteLine($"ukazhite znachenie stepeni (cislo B) > 0");
+}
+
+Console.WriteLine($"chislo {numA} v stepeni {numB} ravno {MathPow(numA, numB)}");
