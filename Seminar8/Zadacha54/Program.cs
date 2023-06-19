4c8de293db9f0aa7835b086904d439b53f51a5b9
Vyacheslav <vyacheslavkargapolov83@gmail.com>
2023-06-19 21:32:53 +0500

    add Seminar8

---
+++ Seminar8/Zadacha54/Program.cs
@@ -0,0 +1,73 @@
+﻿/*Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+*/
+
+using System;
+using static System.Console;
+
+Clear();
+Write("Введите количество строк массива: ");
+int rows = int.Parse(ReadLine());
+Write("Введите количество столбцов массива: ");
+int columns = int.Parse(ReadLine());
+
+int[,] array = GetArray(rows, columns,-10, 10);
+PrintArray(array);
+WriteLine();
+
+//SortedRow(array);
+WriteLine();
+
+int[,] GetArray(int m, int n, int min, int max) //метод создает двумерный массив
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(min, max);
+        }
+
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)  //метод выводит двумерный массив
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i, j], 3} ");
+        }
+        WriteLine();
+    }
+}
+
+void SortedRow(int[,] inArray)
+{
+     for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+           
+                int k = inArray[i, j+1];
+                if (inArray[i, j] < k)
+                {
+                    int l = inArray[i, j];
+                    inArray[i, j] = k;
+                    k = l;
+                    Write(inArray[i,j]);
+                }
+               
+        WriteLine();
+        }
+    }
+}
---
+++ Seminar8/Zadacha56/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задайте прямоугольный двумерный массив. Напишите программу, 
+//которая будет находить строку с наименьшей суммой элементов.
+using System;
+using static System.Console;
+Clear();
+
+Write("Введите количество строк: ");
+int m = int.Parse(ReadLine());
+Write("Введите количество столбцов: ");
+int n = int.Parse(ReadLine());
+int[,] array = GetArray(m, n);
+PrintArray(array);
+
+WriteLine($"Строка с наименьшей суммой - {GetRowNumber(array)} сверху");
+
+int[,] GetArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(-5, 10);
+        }
+
+    }
+    return result;
+}
+
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i, j],3} ");
+        }
+        WriteLine();
+    }
+}
+int GetRowNumber (int [,] array)
+{
+    int row = 0;
+    int minsum = 0;
+    for (int i = 0; i<array.GetLength(1); i++)
+    {
+        minsum = minsum + array[0,i];
+    }
+
+    for (int i = 1; i<array.GetLength(0); i++)
+    {
+        int sum = 0; 
+        for (int j = 0; j<array.GetLength(1); j++)
+        {
+            sum = sum+array[i,j];
+        }
+        if (minsum > sum)
+        {
+            minsum = sum;
+            row = i + 1;
+        }
+    }
+    return row;
+}
