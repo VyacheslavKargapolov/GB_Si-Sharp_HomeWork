8091562023c46fa93423ca9b952987650150313b
Вячеслав <131903728+VyacheslavKargapolov@users.noreply.github.com>
2023-06-20 14:04:40 +0500

    add kod

--- Seminar8/Zadacha58/Program.cs
+++ Seminar8/Zadacha58/Program.cs
@@ -5,3 +5,74 @@
 Результирующая матрица будет:
 18 20
 15 18 */
+
+using System;
+using static System.Console;
+Clear();
+WriteLine("Введите количество строк первой матрицы: ");
+int A = int.Parse(ReadLine());
+WriteLine("Введите количество столбцов первой матрицы: ");
+int B = int.Parse(ReadLine());
+WriteLine("Введите количество строк второй матрицы: ");
+int C = int.Parse(ReadLine());
+WriteLine("Введите количество столбцов второй матрицы: ");
+int D = int.Parse(ReadLine());
+
+int[,] array = GetRandomArray(A, B, 0, 10);
+PrintArray(array);
+WriteLine();
+int[,] array2 = GetRandomArray(C, D, 0, 10);
+PrintArray(array2);
+
+PrintArray(GetRezultrray(array, array2));
+
+int[,] GetRandomArray(int a, int b, int minValue, int maxValue)
+{
+    int[,] result = new int[a, b];
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return result;
+}
+
+
+
+void PrintArray(int[,] arr)
+{ 
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Write($" {arr[i, j]} ");
+        }
+        WriteLine();
+    }
+}
+WriteLine();
+
+if(A != D)
+{
+    WriteLine("Нельзя найти произведение таких матриц");
+}
+
+
+int [,] GetRezultrray(int [,] arr, int [,] arr2)
+{
+    int [,] result = new int [arr.GetLength(0), arr2.GetLength(1)];
+    
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j < arr2.GetLength(1); j++)
+        {
+            for(int k = 0; k < arr.GetLength(1);k++)
+            {
+                result [i,j] += arr[i,k] * arr2[k, j];
+            } 
+        }
+    }
+    return result;
+}
